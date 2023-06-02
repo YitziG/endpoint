eeddb5146df71817f3e1aad0776ecdc181ceb56e
yitzi <el3k6uwq@duck.com>
2023-06-02 10:04:43 +0300

    Refactor code to use consistent spacing.
    The code in this commit refactors the formatting of the imports to use consistent empty spaces and removes some debugging `console.log` statements.

--- App.js
+++ App.js
@@ -1,47 +1,38 @@
 import express from "express";
 import bodyParser from "body-parser";
-import {OpenAI} from "langchain/llms/openai";
-import {APIChain} from "langchain/chains";
-
-import {WIX_API_DOCS} from "./langchain/docs/wix.mjs";
-
+import { OpenAI } from "langchain/llms/openai";
+import { APIChain } from "langchain/chains";
+import { WIX_API_DOCS } from "./langchain/docs/wix.mjs";
 
 // Load environment variables from .env file
-import {config} from "dotenv";
-
+import { config } from "dotenv";
 config();
 
 const app = express();
 
 app.use(bodyParser.json());
-
 app.use((req, res, next) => {
-    console.log(`Request received: ${req.method} ${req.originalUrl}`);
     next();
 });
+
 app.post('/everything', async (req, res) => {
-    console.log('Processing /chat-completions request...');
     const userText = req.body.text;
 
     try {
-        console.log('Tokens for user set successfully');
         // Initialize the OpenAI model with your API key
-        const model = new OpenAI({modelName: "gpt-4", openAIApiKey: process.env.OPENAI_API_KEY});
-        console.log('OpenAI model initialized');
+        const model = new OpenAI({ modelName: "gpt-4", openAIApiKey: process.env.OPENAI_API_KEY });
+        
         // Initialize the APIChain with the OpenAI model and the Wix API documentation
         const chain = APIChain.fromLLMAndAPIDocs(model, WIX_API_DOCS, {
             headers: {
                 Authorization: `Bearer ${process.env.WIX_API_KEY}`, 'wix-site-id': process.env.WIX_SITE_ID,
             }
         });
-        console.log('APIChain initialized');
 
         // Call the chain with the user's text
-        const result = await chain.call({question: userText});
-        console.log('APIChain call result: ', result);
+        const result = await chain.call({ question: userText });
 
         res.send(result.output);
-        console.log('Response sent');
     } catch (error) {
         console.error('Error during request processing: ', error);
         res.status(500).send('Something went wrong');
